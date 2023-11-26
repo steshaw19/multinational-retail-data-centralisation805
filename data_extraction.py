5ffa0acb6cc2b2e9700270343fe0f0dcb6c12a21
Ste <steshaw19@gmail.com>
2023-11-26 11:49:00 +0000

    Adds method to list database tables.

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,30 @@
+from sqlalchemy import inspect
+from database_utils import DatabaseConnector
+
 class DataExtractor:
-    def __init__(self):
-        # configurations will be initialised here
-        pass
+    def list_db_tables(self):
+        try:
+            # Instantiate DatabaseConnector to access its methods
+            db_connector = DatabaseConnector()
+
+            # Get credentials
+            credentials = db_connector.read_db_creds()
+
+            # Initialize the database engine
+            engine = db_connector.init_db_engine(credentials)
+
+            # Use the inspector to get table names
+            inspector = inspect(engine)
+            table_names = inspector.get_table_names()
+
+            return table_names
+        except Exception as e:
+            print(f"Error listing tables: {e}")
+            return None
+
+# Create an instance of DataExtractor
+extractor = DataExtractor()
+
+# Call the list_db_tables method
+tables = extractor.list_db_tables()
+print(tables)
