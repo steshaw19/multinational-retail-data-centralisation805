6782bdc29333d8b15198828d9fc475e6057db475
Ste <steshaw19@gmail.com>
2023-11-26 12:39:23 +0000

    Adds a read_rds_table method to read user data.

--- data_extraction.py
+++ data_extraction.py
@@ -2,18 +2,27 @@ import pandas as pd
 from sqlalchemy import inspect
 from database_utils import DatabaseConnector
 
+# Instantiate DatabaseConnector to access its methods
+db_connector = DatabaseConnector()
+
+# Get credentials
+credentials = db_connector.read_db_creds()
+
+# Initialize the database engine
+engine = db_connector.init_db_engine(credentials)
+
 class DataExtractor:
-    def list_db_tables(self):
+    def read_rds_table(self, db_connector, engine, table_name):
         try:
-            # Instantiate DatabaseConnector to access its methods
-            db_connector = DatabaseConnector()
+            query = f"SELECT * FROM {table_name}"
+            df = pd.read_sql_query(query, engine, index_col='index')
+            return df
+        except Exception as e:
+            print(f"Error reading table {table_name}: {e}")
+            return None
 
-            # Get credentials
-            credentials = db_connector.read_db_creds()
-
-            # Initialize the database engine
-            engine = db_connector.init_db_engine(credentials)
-
+    def list_db_tables(self, engine):
+        try:
             # Use the inspector to get table names
             inspector = inspect(engine)
             table_names = inspector.get_table_names()
@@ -27,5 +36,9 @@ class DataExtractor:
 extractor = DataExtractor()
 
 # Call the list_db_tables method
-tables = extractor.list_db_tables()
+tables = extractor.list_db_tables(engine)
 print(f"TABLES: {tables}")
+
+user_data_df = extractor.read_rds_table(db_connector, engine, 'legacy_users')
+print("User Data DataFrame:")
+print(user_data_df)
