beae50ad30fe8a2e25c4e5f868f870d96a262d02
Ste <steshaw19@gmail.com>
2023-11-27 12:39:46 +0000

    Adds tested data_cleaning file.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,21 @@
 import pandas as pd
 import numpy as np
 from data_extraction import user_data_df
+from database_utils import DatabaseConnector
+from data_extraction import DataExtractor
+
+# Instantiate DatabaseConnector to access its methods
+db_connector = DatabaseConnector()
+
+# Get credentials
+credentials = db_connector.read_db_creds()
+
+# Initialize the database engine
+engine = db_connector.init_db_engine(credentials)
+
+# Create an instance of DataExtractor
+extractor = DataExtractor()
+
 
 class DataCleaning:
     def clean_user_data(self, user_data_df):
@@ -14,8 +29,9 @@ class DataCleaning:
             # Filter rows where the user ID has the desired length
             user_data_df = user_data_df[user_data_df['user_uuid'].str.len() == 36]
 
-            # Drop any names that contain numbers
-            user_data_df = user_data_df[user_data_df['first_name', 'last_name'].str.isalpha()]
+            # Drop any names that contain numbers but checks if the name is NULL first
+            user_data_df = user_data_df[user_data_df[['first_name', 'last_name']].notnull().all(axis=1) &
+                            user_data_df[['first_name', 'last_name']].applymap(lambda x: x.isalpha()).all(axis=1)]
 
             # Removes email address that are invalid (does not have an @ sign)
             mask = user_data_df['email_address'].str.contains('@', na=False)
@@ -32,22 +48,22 @@ class DataCleaning:
             user_data_df = user_data_df[no_number_mask_country]
 
             # Create a boolean mask for rows with numbers in the 'column' column
-            no_number_mask_company = ~user_data_df['column'].str.contains(r'\d', na=False)
+            no_number_mask_company = ~user_data_df['company'].str.contains(r'\d', na=False)
 
             # Filter the DataFrame using the boolean mask
             user_data_df = user_data_df[no_number_mask_company]
 
             # Create a boolean mask for rows with valid country codes (2 or 3 characters)
-            valid_country_code_mask = user_data_df['country'].str.len().isin([2, 3])
+            valid_country_code_mask = user_data_df['country_code'].str.len().isin([2, 3])
 
             # Filter the DataFrame using the boolean mask
             user_data_df = user_data_df[valid_country_code_mask]
 
-            # Create a boolean mask for rows with phone numbers containing letters (excluding 'ext')
-            valid_phone_mask = ~user_data_df['phone_number'].str.contains(r'[^0-9extEXT]', na=False)
+            # # Create a boolean mask for rows with phone numbers containing letters (excluding 'ext')
+            # valid_phone_mask = ~user_data_df['phone_number'].str.contains(r'[^0-9extEXT()+.-'']', na=False)
 
-            # Filter the DataFrame using the boolean mask
-            user_data_df = user_data_df[valid_phone_mask]
+            # # Filter the DataFrame using the boolean mask
+            # user_data_df = user_data_df[valid_phone_mask]
 
             return user_data_df
         
@@ -60,6 +76,8 @@ if __name__ == '__main__':
     cleaned_data = data_cleaner.clean_user_data(user_data_df)
     print("Cleaned Data:")
     print(cleaned_data)
+    duplicates = cleaned_data[cleaned_data.index.duplicated()]
+    print(f"Duplicate rows: {duplicates}")
     
             # Find duplicates based off unique user id            
             # This indicated that the NULL values were the only duplicates and could be removed.
