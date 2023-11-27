79c6d950e122321bd5d3d8ddcb891f3dd446e2a1
Ste <steshaw19@gmail.com>
2023-11-27 12:40:40 +0000

    Adds instances of database connector to initialise inside a method in DataExtractor class.

--- data_extraction.py
+++ data_extraction.py
@@ -2,44 +2,40 @@ import pandas as pd
 from sqlalchemy import inspect
 from database_utils import DatabaseConnector
 
-# Instantiate DatabaseConnector to access its methods
-db_connector = DatabaseConnector()
-
-# Get credentials
-credentials = db_connector.read_db_creds()
-
-# Initialize the database engine
-engine = db_connector.init_db_engine(credentials)
-
 class DataExtractor:
-    def read_rds_table(self, db_connector, engine, table_name):
+    def __init__(self):
+        # Instantiate DatabaseConnector to access its methods
+        self.db_connector = DatabaseConnector()
+        # Get credentials
+        self.credentials = self.db_connector.read_db_creds()
+        # Initialize the database engine
+        self.engine = self.db_connector.init_db_engine(self.credentials)
+
+    def read_rds_table(self, table_name):
         try:
             query = f"SELECT * FROM {table_name}"
-            df = pd.read_sql_query(query, engine, index_col='index')
+            df = pd.read_sql_query(query, self.engine, index_col='index')
             return df
         except Exception as e:
             print(f"Error reading table {table_name}: {e}")
             return None
 
-    def list_db_tables(self, engine):
+    def list_db_tables(self):
         try:
             # Use the inspector to get table names
-            inspector = inspect(engine)
+            inspector = inspect(self.engine)
             table_names = inspector.get_table_names()
-
             return table_names
         except Exception as e:
             print(f"Error listing tables: {e}")
             return None
 
-if __name__== '__main__':
-    # Create an instance of DataExtractor
-    extractor = DataExtractor()
+# Instantiate DataExtractor to access its methods
+extractor = DataExtractor()
 
-    # Call the list_db_tables method
-    tables = extractor.list_db_tables(engine)
-    print(f"TABLES: {tables}")
+# Read data from the RDS table (example table name: 'legacy_users')
+user_data_df = extractor.read_rds_table('legacy_users')
 
-    user_data_df = extractor.read_rds_table(db_connector, engine, 'legacy_users')
-    print("User Data DataFrame:")
-    print(user_data_df)
+# Print the DataFrame
+print("User Data DataFrame:")
+print(user_data_df)
